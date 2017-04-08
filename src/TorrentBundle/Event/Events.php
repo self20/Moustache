2fe546ae317c3c8bebeb462d7a4174aa58cd5012
gui-don <archaygo@gmail.com>
2017-04-08 19:26:33 +0200

    refactoring => Renames events

--- src/TorrentBundle/Event/Events.php
+++ src/TorrentBundle/Event/Events.php
@@ -6,17 +6,17 @@ namespace TorrentBundle\Event;
 
 class Events
 {
-    const AFTER_CLIENT_INITIALIZED = 'event.client.after.initialize';
+    const AFTER_CLIENT_INITIALIZED = 'torrent.client.after.initialize';
 
-    const AFTER_TORRENT_ADDED = 'event.torrent.after.added';
+    const AFTER_TORRENT_ADDED = 'torrent.torrent.after.added';
 
-    const AFTER_TORRENT_GET = 'event.torrent.after.get';
+    const AFTER_TORRENT_GET = 'torrent.torrent.after.get';
 
-    const AFTER_TORRENT_REMOVED = 'event.torrent.after.removed';
+    const AFTER_TORRENT_REMOVED = 'torrent.torrent.after.removed';
 
-    const AFTER_TORRENT_PAUSED = 'event.torrent.after.paused';
+    const AFTER_TORRENT_PAUSED = 'torrent.torrent.after.paused';
 
-    const AFTER_TORRENT_STARTED = 'event.torrent.after.started';
+    const AFTER_TORRENT_STARTED = 'torrent.torrent.after.started';
 
-    const AFTER_TORRENT_STOPPED = 'event.torrent.after.stopped';
+    const AFTER_TORRENT_STOPPED = 'torrent.torrent.after.stopped';
 }
