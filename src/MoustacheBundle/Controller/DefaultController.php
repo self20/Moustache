b9f366f417a2ea3dc9afc3ef2e6a6947e558168d
gui-don <archaygo@gmail.com>
2017-04-15 10:16:07 +0200

    tech => Update composer

--- src/MoustacheBundle/Controller/DefaultController.php
+++ src/MoustacheBundle/Controller/DefaultController.php
@@ -11,16 +11,13 @@ use Symfony\Component\Templating\EngineInterface;
 use TorrentBundle\Client\ClientInterface;
 use TorrentBundle\Entity\Torrent;
 
-// @HEYLISTEN Upload a torrent with an URL too
 // @HEYLISTEN Ajouter la fonction de remove de torrent dans l’UI (dans un second temps OK).
 // @HEYLISTEN Chercher fichier localement ?
-// @HEYLISTEN Classer les exceptions en configuration ou BugError
 
 // @HEYLISTEN Solution: pour le download, faire un lien symbolique dans web (ex: /download/{token}/filename.mp3), avec un token unique, généré par fichier;
 // Les liens sont supprimés au login/logout -> minimum (mais avec fileatime() aussi). Au mieux, utiliser fileatime() dans une commande cron; Après 2 minutes depuis le dernier accès, supprimer le lien symbolique.
 // Expliquer dans la doc le besoin d’ajouter le cron.
 // @HEYLISTEN Sécurité => Faire très attention concernant le download de fichier torrent: faire une liste blanche de chose downloadable => dossier/zip, mkv, etc. pour ne pas que des fichiers PHP, py ou autre deviennent dispo et exécutables…
-// @HEYLISTEN Error images optimisations
 
 class DefaultController
 {
