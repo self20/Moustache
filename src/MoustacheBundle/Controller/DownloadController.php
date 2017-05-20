f96058d967711df74ee48a1fedb346317e35b730
gui-don <archaygo@gmail.com>
2017-05-20 16:19:08 +0200

    tech => Adds very early dev for direct download of files

---
+++ src/MoustacheBundle/Controller/DownloadController.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MoustacheBundle\Controller;
+
+use MoustacheBundle\Service\Redirector;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use TorrentBundle\Client\ClientInterface;
+use TorrentBundle\Entity\TorrentInterface;
+
+class DownloadController
+{
+    use TorrentGetterTrait;
+
+    /**
+     * @var ClientInterface
+     */
+    private $torrentClient;
+
+    /**
+     * @var Redirector
+     */
+    private $redirector;
+
+    /**
+     * @param ClientInterface $torrentClient
+     * @param Redirector $redirector
+     */
+    public function __construct(ClientInterface $torrentClient, Redirector $redirector)
+    {
+        $this->torrentClient = $torrentClient;
+        $this->redirector = $redirector;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function downloadAction(int $id): string
+    {
+        $torrent = $this->getSingleTorrent($id);
+        $this->isDownloadable($torrent);
+    }
+
+    private function isDownloadable(TorrentInterface $torrent): bool
+    {
+        if (!$torrent->isFile()) {
+            throw new BadRequestHttpException('This torrent cannot be downloaded because it‘s a directory.');
+        }
+    }
+}
---
+++ test/Functional/MoustacheBundle/08-download.feature
@@ -0,0 +1,15 @@
+@Scenario
+@Download
+Feature: Ability to download a torrent
+
+    Background:
+        Given I am authenticated as "normal" with password "test"
+
+#    Scenario: As Moustachor, I cannot see a download button for directories
+#        When I am on "/"torrent-3
+#        And I should see 9 ".card" elements
+
+    Scenario: As Moustachor, I see an error if I try to download a directory directly
+        When I go to "/download/3"
+        Then the response status code should be 400
+        Then I should see "torrent cannot be downloaded"
