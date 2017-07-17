474b06fc7cb044029aa7d32dc4e12df8bd0f0c08
gui-don <archaygo@gmail.com>
2017-07-17 20:56:59 +0200

    doc => Fixes some PHPDoc

--- src/MoustacheBundle/Service/CanAddMessage.php
+++ src/MoustacheBundle/Service/CanAddMessage.php
@@ -7,26 +7,30 @@ namespace MoustacheBundle\Service;
 interface CanAddMessage
 {
     /**
-     * @param string   $message
-     * @param ...mixed $parameters
+     * @param string $message
+     * @param mixed  $parameters
+     * @param mixed  $...
      */
     public function addErrorMessage(string $message, ...$parameters);
 
     /**
-     * @param string   $message
-     * @param ...mixed $parameters
+     * @param string $message
+     * @param mixed  $parameters
+     * @param mixed  $...
      */
     public function addInfoMessage(string $message, ...$parameters);
 
     /**
-     * @param string   $message
-     * @param ...mixed $parameters
+     * @param string $message
+     * @param mixed  $parameters
+     * @param mixed  $...
      */
     public function addSuccessMessage(string $message, ...$parameters);
 
     /**
-     * @param string   $message
-     * @param ...mixed $parameters
+     * @param string $message
+     * @param mixed  $parameters
+     * @param mixed  $...
      */
     public function addWarnMessage(string $message, ...$parameters);
 }
