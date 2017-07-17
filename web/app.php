d0903789ac1dc64e210b9be7a5ca433eb6b289cb
gui-don <archaygo@gmail.com>
2017-07-17 21:27:38 +0200

    refactoring => Cleans app.php file

--- web/app.php
+++ web/app.php
@@ -2,10 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-if (preg_match('/\.(?:png|jpg|jpeg|css|js|woff2|woff|svg|ttf|ico|gif)$/', $_SERVER['PHP_SELF'])) {
-    return false;
-}
-
 /**
  * @var Composer\Autoload\ClassLoader
  */
@@ -14,10 +10,7 @@ include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
