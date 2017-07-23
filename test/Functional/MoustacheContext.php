6c6932b670f710be47494daa8d15096710f1edcd
gui-don <archaygo@gmail.com>
2017-07-23 15:19:27 +0200

    miss => Adds some context method for functional tests

--- test/Functional/MoustacheContext.php
+++ test/Functional/MoustacheContext.php
@@ -4,6 +4,7 @@ use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\MinkExtension\Context\MinkContext;
 use Behat\Symfony2Extension\Context\KernelDictionary;
+use TorrentBundle\Entity\Torrent;
 
 class MoustacheContext extends MinkContext implements Context, SnippetAcceptingContext
 {
@@ -12,4 +13,51 @@ class MoustacheContext extends MinkContext implements Context, SnippetAcceptingC
     use DatabaseContextTrait;
     use DebugContextTrait;
     use RedirectionContextTrait;
+
+    /**
+     * @Given /^The maintenance lock is present$/
+     */
+    public function theMaintenanceLockIsPresent()
+    {
+        $maintenancelockFile = $this->getContainer()->getParameter('maintenance_lock_file');
+
+        touch($maintenancelockFile);
+    }
+
+    /**
+     * @Given /^The maintenance lock is absent$/
+     */
+    public function theMaintenanceLockIsAbsent()
+    {
+        $maintenancelockFile = $this->getContainer()->getParameter('maintenance_lock_file');
+
+        if (file_exists($maintenancelockFile)) {
+            unlink($maintenancelockFile);
+        }
+    }
+
+    /**
+     * @When /^(?:|I )add a new torrent in database$/
+     */
+    public function iAddATorrentInDatabase()
+    {
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $userRepository = $this->getContainer()->get('torrent.repository.user');
+
+        $torrent = new Torrent();
+
+        $torrent->setHash('missing');
+        $torrent->setUser($userRepository->findOneBy(['username' => 'normal']));
+        $torrent->setName('missing');
+        $torrent->setDownloadDir('/var/downloads');
+        $torrent->setDownloadRate(0);
+        $torrent->setUploadRate(0);
+        $torrent->setNbPeers(0);
+        $torrent->setTotalByteSize(128000000);
+        $torrent->setCurrentByteSize(0);
+        $torrent->setFriendlyName('missing');
+
+        $entityManager->persist($torrent);
+        $entityManager->flush();
+    }
 }
