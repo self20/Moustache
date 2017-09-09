c362d1982257fe9f395c03d43c21f20889962435
gui-don <archaygo@gmail.com>
2017-09-09 11:35:56 +0200

    feat: adapts torrentMenu form so it can handle magnet links

--- src/MoustacheBundle/Form/TorrentMenu.php
+++ src/MoustacheBundle/Form/TorrentMenu.php
@@ -4,33 +4,67 @@ declare(strict_types=1);
 
 namespace MoustacheBundle\Form;
 
+use MoustacheBundle\Form\DataTransformer\UrlToMagnetLinkTransformer;
 use MoustacheBundle\Form\DataTransformer\UrlToUploadedFileTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use TorrentBundle\Entity\Torrent;
 
 class TorrentMenu extends AbstractType
 {
     /**
+     * @var UrlToUploadedFileTransformer
+     */
+    private $urlToUploadedFilTransformer;
+
+    /**
+     * @var UrlToMagnetLinkTransformer
+     */
+    private $urlToMagnetLinkTransformer;
+
+    /**
+     * @param UrlToUploadedFileTransformer $urlToUploadedFilTransformer
+     * @param UrlToMagnetLinkTransformer   $urlToMagnetLinkTransformer
+     */
+    public function __construct(UrlToUploadedFileTransformer $urlToUploadedFilTransformer, UrlToMagnetLinkTransformer $urlToMagnetLinkTransformer)
+    {
+        $this->urlToUploadedFilTransformer = $urlToUploadedFilTransformer;
+        $this->urlToMagnetLinkTransformer = $urlToMagnetLinkTransformer;
+    }
+
+    /**
      * @param FormBuilderInterface $builder
      * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('uploadedFile', FileType::class, [
-                'data_class' => null,
-                'required' => false,
-            ])
-            ->add('uploadedFileByUrl', TextType::class, [
-                'data_class' => null,
-                'required' => false,
-            ])
-            ->get('uploadedFileByUrl')->addModelTransformer(new UrlToUploadedFileTransformer())
-        ;
+        $builder->add('uploadedFile', FileType::class, [
+            'data_class' => null,
+            'required' => false,
+        ]);
+        $builder->add('uploadedFileByUrl', TextType::class, [
+            'data_class' => null,
+            'required' => false,
+        ]);
+        $builder->add('magnetLink', HiddenType::class, [
+            'data_class' => null,
+            'required' => false,
+        ]);
+
+        $builder->get('uploadedFileByUrl')->addModelTransformer($this->urlToUploadedFilTransformer);
+        $builder->get('magnetLink')->addModelTransformer($this->urlToMagnetLinkTransformer);
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $data = $event->getData();
+            $data['magnetLink'] = $data['uploadedFileByUrl'] ?? '';
+            $event->setData($data);
+        });
     }
 
     /**
